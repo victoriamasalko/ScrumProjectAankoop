6caff169689c78226aaadead37d625cb3abe5ebe
Jeroen Marly <jeroen.marly@hotmail.com>
2026-06-19 14:19:53 +0200

    Het opvragen van een leverancier en leveranciers in de repository interface

---
+++ Data/Repositories/ILeverancierRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories
+{
+    public interface ILeverancierRepository
+    {
+        Task<Leverancier?> GetLeverancierById(int id);
+
+        Task<IEnumerable<Leverancier>> GetLeveranciers();
+    }
+}
