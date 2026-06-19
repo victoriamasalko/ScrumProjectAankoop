30f86418c0de143fb272f39d68420c27725a6712
Danylo <danylo.yahnichenko@vdabcampus.be>
2026-06-19 15:26:21 +0200

    Voeg ICategorieRepository toe. Voeg GetCategorieen method toe aan ICategorieRepository. Voeg CategorieService toe.

---
+++ Data/Repositories/ICategorieRepository.cs
@@ -0,0 +1,11 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories;
+
+public interface ICategorieRepository
+{
+    Task<IEnumerable<Categorie>> GetCategorieen(); 
+}
