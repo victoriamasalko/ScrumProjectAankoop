44cecc490915a7ccda3de56a6dfde784ec3802d4
Danylo <danylo.yahnichenko@vdabcampus.be>
2026-06-19 14:34:55 +0200

    Voeg IArtikelRepository toe. Voeg GetArtikel en GetArtikelById methods toe aan IArtikelRepository

---
+++ Data/Repositories/IArtikelRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories
+{
+    internal interface IArtikelRepository
+    {
+        Task<IEnumerable<Artikel>> GetArtikels();
+
+        Task<Artikel?> GetArtikelById(int  id);
+    }
+}
