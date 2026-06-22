8f74063ce62755308a82f326468966b0ecdce7b5
Victoria Masalkovski <victoria.masalkovski@gmail.com>
2026-06-22 11:41:32 +0200

    Aanmaak LeverancierController met Index en GetLeveranciers Aanmaak LeverancierOveriewViewModel

---
+++ Web/Controllers/LeverancierController.cs
@@ -0,0 +1,32 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models.ViewModels;
+
+namespace Web.Controllers
+{
+    public class LeverancierController : Controller
+    {
+        private readonly LeverancierService leverancierService;
+        public LeverancierController(LeverancierService leverancierService)
+        {
+            this.leverancierService = leverancierService;
+        }
+        //Overzicht van de leveranciers tonen
+        public async Task<IActionResult> Index()
+        {
+            LeverancierOverviewViewModel viewModel = await GetLeveranciers();
+            return View(viewModel);
+        }
+        //Methode GetLeveranciers met lijst van Leveranciers
+        public async Task <LeverancierOverviewViewModel> GetLeveranciers()
+        {
+            IEnumerable<Leverancier> leveranciers = await leverancierService.GetLeveranciers();
+
+            return new LeverancierOverviewViewModel
+            {
+                Leveranciers = leveranciers
+            };
+        }
+
+    }
+}
---
+++ Web/Models/ViewModels/LeverancierOverviewViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Data.Models;
+
+namespace Web.Models.ViewModels
+{
+    public class LeverancierOverviewViewModel
+    { 
+        //Navigatie naar Leverancier voor de properties Naam, VoornaamContactpersoon en FamilienaamContactpersoon.
+        public IEnumerable<Leverancier> Leveranciers { get; set; }
+        //Navigatie naar Artikel voor de artikelnaam.
+        public IEnumerable<Artikel> Artikelen { get; set; }
+        public string FilterOpArtikel { get; set; }
+    }
+}
