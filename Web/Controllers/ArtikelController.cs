855fba3ac59bc1702b9d6ffdb7ac70af4418fcc5
Jeroen Marly <jeroen.marly@hotmail.com>
2026-06-22 10:31:31 +0200

    Add ArtikelController

---
+++ Web/Controllers/ArtikelController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ArtikelController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
