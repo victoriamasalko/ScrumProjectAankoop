62792f4ff17f64dbb3d3d639904748588e47e4d0
Jeroen Marly <jeroen.marly@hotmail.com>
2026-06-22 10:33:30 +0200

    Inject ArtikelService into ArtikelController

--- Web/Controllers/ArtikelController.cs
+++ Web/Controllers/ArtikelController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Service;
 
 namespace Web.Controllers
 {
     public class ArtikelController : Controller
     {
+        private readonly ArtikelService artikelService;
+
+        public ArtikelController(ArtikelService artikelService)
+        {
+            this.artikelService = artikelService;
+        }
 
         public IActionResult Index()
         {
