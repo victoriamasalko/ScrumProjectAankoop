d3def8bfeec71bd99666abfb7d6318bfd073c88b
Victoria Masalkovski <victoria.masalkovski@gmail.com>
2026-06-23 15:46:27 +0200

    LeverancierController en LeverancierOverzichtViewModel aangepast consistent aan Artikel

--- Web/Controllers/LeverancierController.cs
+++ Web/Controllers/LeverancierController.cs
@@ -11,19 +11,20 @@ namespace Web.Controllers
         {
             this.leverancierService = leverancierService;
         }
-        //Overzicht van leveranciers tonen.
-        //Je neemt de leveranciers uit de database via de service en stopt deze in de variabele leveranciers.
-        //Deze wordt in de Leveranciers property van het ViewModel gestoken.
+        
         public async Task<IActionResult> Index()
         {
             var leveranciers = await leverancierService.GetLeveranciersAsync();
 
-            var viewModel = new LeverancierOverviewViewModel
+            var viewModel = leveranciers.Select(l => new LeverancierOverviewViewModel
             {
-                Leveranciers = leveranciers
-            };
+                Naam = l.Naam,
+                BtwNummer = l.BtwNummer,
+                VoornaamContactpersoon = l.VoornaamContactpersoon,
+                FamilienaamContactpersoon = l.FamilienaamContactpersoon
+            }).ToList();
 
-            return View(viewModel);
+            return View(nameof(Index), viewModel);
 
         }
     }
