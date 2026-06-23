69af1a31d9e991189957d29f8de6837f3c7df000
= <gert.verstrepen@vdabcampus.be>
2026-06-23 09:51:44 +0200

    Samenbrengen wijzigingen feature branch A100-1_A100-3_ArtikelController en feature branch A100-2-Artikel-Aanmaken-Overview

--- Web/Models/ViewModels/ArtikelOverviewViewModel.cs
+++ Web/Models/ViewModels/ArtikelOverviewViewModel.cs
@@ -1,19 +1,25 @@
 ﻿using Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Models.ViewModels;
 
 public class ArtikelOverviewViewModel
 {
+    public int ArtikelId { get; set; }
+
     public string Naam { get; set; }
 
     public string Ean { get; set; }
 
-    public IEnumerable<String> Categorieen { get; set; }
+    public IEnumerable<String> Categorieen { get; set; } = [];
 
+    [DisplayFormat(DataFormatString = "{0:€ #,##0.00}")]
     public decimal Prijs { get; set; }
 
     public int Voorraad { get; set; }
 
     public int AantalBesteldLeverancier { get; set; }
+
+    public int LeveranciersId { get; set; }
 
 }
