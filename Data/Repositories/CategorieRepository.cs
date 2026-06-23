b5fac004b685fc11a8483c1a7281e464371949c0
= <gert.verstrepen@vdabcampus.be>
2026-06-24 00:03:44 +0200

    A300-1 Artikel categorieën - Backend - Model "Categorie" : navigation property hernoemd van "InverseHoofdCategorie" naar "Subcategorieen" + PrulariacomContext overeenkomstig aangepast - nodige methods toegevoegd aan ICategorieRepository en CategorieRepository en CategorieService - CategorieController aangemaakt met injectie van CategorieService en "Index" actie toegevoegd - CategorieOverviewViewModel toegevoegd - minimale view "Index" toegevoegd - _Layout.cshtml gecorrigeerd zodat naar juiste controllers/actions wordt verwezen

--- Data/Repositories/CategorieRepository.cs
+++ Data/Repositories/CategorieRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,8 +15,79 @@ public class CategorieRepository : ICategorieRepository
         this.context = context;
     }
 
-    public Task<IEnumerable<Categorie>> GetCategorieenAsync()
+    public async Task<Categorie?> GetCategorieByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await context.Categorieen
+            .Include(c => c.HoofdCategorieId)
+            .Include(c => c.SubCategorieen)
+            .Include(c => c.Artikels)
+            .FirstOrDefaultAsync(c => c.CategorieId == id);
     }
+
+    public async Task<IEnumerable<Categorie>> GetCategorieenAsync()
+    {
+        return await context.Categorieen
+            .Include(c => c.HoofdCategorie)
+            .Include(c => c.SubCategorieen)
+            .Include(c => c.Artikels)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Categorie>> GetCategorieenByArtikelIdAsync(int id)
+    {
+        return await context.Categorieen
+            .Include(c => c.HoofdCategorie)
+            .Include(c => c.SubCategorieen)
+            .Where(c => c.Artikels.Any(a => a.ArtikelId == id))
+            .ToListAsync();
+    }
+
+    public async Task<Categorie> UpdateCategorieAsync(Categorie categorie)
+    {
+        context.Categorieen.Update(categorie);
+        await context.SaveChangesAsync();
+        return categorie;
+    }
+
+    public async Task<Categorie> AddArtikelToCategorieAsync(Categorie categorie, int artikelId)
+    {
+        if (categorie.Artikels.Any(a => a.ArtikelId == artikelId))
+        {
+            return categorie;
+        }
+
+        var artikel = await context.Artikels.FindAsync(artikelId);
+        if (artikel == null)
+        {
+            throw new KeyNotFoundException($"Het artikel met ArtikelId = {artikelId} bestaat niet");
+        }
+
+        categorie.Artikels.Add(artikel);
+        await context.SaveChangesAsync();
+
+        return categorie;
+    }
+
+    public async Task<Categorie> RemoveArtikelFromCategorieAsync(Categorie categorie, int artikelId)
+    {
+        var gekoppeldArtikel = categorie.Artikels.FirstOrDefault(a => a.ArtikelId == artikelId);
+
+        if (gekoppeldArtikel == null)
+        {
+            throw new KeyNotFoundException($"Het artikel met ArtikelId = {artikelId} is niet gekoppeld aan de categorie {categorie.Naam} en kon dus niet ontkoppeld worden");
+        }
+
+        categorie.Artikels.Remove(gekoppeldArtikel);
+        await context.SaveChangesAsync();
+
+        return categorie;
+    }
+
+    public async Task<Categorie> UpdateCategorie(Categorie categorie)
+    {
+        context.Categorieen.Update(categorie);
+        await context.SaveChangesAsync();
+        return categorie;
+    }
+
 }
