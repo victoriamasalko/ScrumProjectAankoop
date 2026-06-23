d236040661a751ce3ffb19edb1f9620f781fdad9
Jeroen Marly <jeroen.marly@hotmail.com>
2026-06-23 13:25:05 +0200

    Add PlaatsService

---
+++ Service/PlaatsService.cs
@@ -0,0 +1,20 @@
+﻿using Data.Models;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class PlaatsService
+    {
+        private readonly IPlaatsRepository plaatsRepository;
+
+        public PlaatsService(IPlaatsRepository plaatsRepository)
+        {
+            this.plaatsRepository = plaatsRepository;
+        }
+
+        public async Task<IEnumerable<Plaats>> GetPlaatsenAsync() => await plaatsRepository.GetPlaatsenAsync();
+    }
+}
