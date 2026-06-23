8f9961147bd18ddca29363604c90bbe26491d6f2
Jeroen Marly <jeroen.marly@hotmail.com>
2026-06-23 13:23:16 +0200

    Add PlaatsRepository

---
+++ Data/Repositories/PlaatsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class PlaatsRepository : IPlaatsRepository
+{
+    private readonly PrulariacomContext _context;
+
+    public PlaatsRepository(PrulariacomContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Plaats>> GetPlaatsenAsync() => await _context.Plaatsen.ToListAsync();
+}
