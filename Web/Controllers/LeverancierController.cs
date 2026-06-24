99207cf4f15b023dc4741b736ba645f7f2515dd9
Victoria Masalkovski <victoria.masalkovski@gmail.com>
2026-06-24 11:40:04 +0200

    LeverancierController aangepast

--- Web/Controllers/LeverancierController.cs
+++ Web/Controllers/LeverancierController.cs
@@ -1,8 +1,9 @@
 ﻿using Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Service;
 using Service;
 using Web.Models.ViewModels;
-using Service;
 
 namespace Web.Controllers
 {
@@ -43,6 +44,59 @@ namespace Web.Controllers
             }
 
             return View(leverancier);
+        }
+        [NonAction]
+        public async Task<List<SelectListItem>> GetPlaatsenAsync()
+        {
+            var plaatsen = await plaatsService.GetPlaatsenAsync();
+
+            return plaatsen.Select(plaats => new SelectListItem()
+            {
+                Text = plaats.Naam,
+                Value = plaats.PlaatsId.ToString()
+            }).ToList();
+
+        }
+
+        // Deze action method geeft het formulier terug waarin een nieuwe leverancier aangemaakt wordt.
+        public async Task<IActionResult> AddLeverancierAsync()
+        {
+            return View("AddLeverancierModal", new AddLeverancierViewModel()
+            {
+                Plaatsen = await GetPlaatsenAsync()
+            });
+        }
+
+        // Deze action method voegt een nieuwe leverancier toe.
+        [HttpPost]
+        public async Task<IActionResult> AddLeverancierAsync(AddLeverancierViewModel addLeverancierViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Maak een Leverancier object op basis van de form.
+                Leverancier leverancier = new Leverancier()
+                {
+                    Naam = addLeverancierViewModel.Naam,
+                    BtwNummer = addLeverancierViewModel.BtwNummer,
+                    Straat = addLeverancierViewModel.Straat,
+                    HuisNummer = addLeverancierViewModel.HuisNummer,
+                    Bus = addLeverancierViewModel.Bus,
+                    VoornaamContactpersoon = addLeverancierViewModel.VoornaamContactpersoon,
+                    FamilienaamContactpersoon = addLeverancierViewModel.FamilienaamContactperoon,
+                    PlaatsId = addLeverancierViewModel.PlaatsId
+                };
+
+                // Roep de service op.
+                await leverancierService.AddLeverancierAsync(leverancier);
+
+                // Redirect naar de overzichtpagina voor de leveranciers.
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                addLeverancierViewModel.Plaatsen = await GetPlaatsenAsync();
+                return View("AddLeverancierModal", addLeverancierViewModel);
+            }
         }
     }
 }
