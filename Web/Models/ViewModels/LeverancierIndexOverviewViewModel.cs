435d25dafa6dbe6c0cdb94dfce3c99c15b9db8dd
MerijnDefoort <merijn.defoort2@gmail.com>
2026-06-30 11:01:46 +0200

    Artikels filter toegevoegd + aanpassingen aan de viewmodels zodat artikels meegegeven kan worden

---
+++ Web/Models/ViewModels/LeverancierIndexOverviewViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Data.Models;
+
+namespace Web.Models.ViewModels
+{
+    public class LeverancierIndexViewModel
+    {
+        public List<LeverancierOverviewViewModel> Leveranciers { get; set; } = [];
+
+        public List<Artikel> Artikels { get; set; } = [];
+    }
+}
