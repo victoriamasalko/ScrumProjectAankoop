91ac5978dcf671a5db73c683dfd63db2642557f1
Laurens <laurens.peeters@vdabcampus.be>
2026-06-30 11:01:10 +0200

    Frontend dummy

--- Web/Controllers/ActiecodeController.cs
+++ Web/Controllers/ActiecodeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
@@ -6,7 +7,32 @@ namespace Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var code1 = new Actiecode
+            {
+                Naam = "Actie1",
+                GeldigVanDatum = DateTime.Today,
+                GeldigTotDatum = DateTime.Today.AddMonths(1),
+                IsEenmalig = true
+            };
+
+            var code2 = new Actiecode
+            {
+                Naam = "Actie2",
+                GeldigVanDatum = DateTime.Today,
+                GeldigTotDatum = DateTime.Today.AddMonths(2),
+                IsEenmalig = false
+            };
+
+            var lijst = new List<Actiecode>();
+
+            if (lijst == null)
+                throw new ArgumentNullException(nameof(lijst));
+
+            lijst.Add(code1);
+            lijst.Add(code2);
+
+
+            return View(lijst);
         }
     }
 }
