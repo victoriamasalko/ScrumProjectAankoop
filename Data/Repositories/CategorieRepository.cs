885e4d6bb17522e91cf8b9fbef158192e3cc4caa
Victoria Masalkovski <victoria.masalkovski@gmail.com>
2026-07-02 12:19:12 +0200

    Aanpassing GetCategorieByIdAsync

--- Data/Repositories/CategorieRepository.cs
+++ Data/Repositories/CategorieRepository.cs
@@ -19,7 +19,7 @@ public class CategorieRepository : ICategorieRepository
     public async Task<Categorie?> GetCategorieByIdAsync(int id)
     {
         return await context.Categorieen
-            .Include(c => c.HoofdCategorieId)
+            .Include(c => c.HoofdCategorie)
             .Include(c => c.SubCategorieen)
             .Include(c => c.Artikels)
             .FirstOrDefaultAsync(c => c.CategorieId == id);
